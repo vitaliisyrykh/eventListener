3588edcba2dd1b7341aa5f1c969bde974f547db5
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-02-08 17:04:25 +0200

    add swap text

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 'use strict';
 
-const btn = document.querySelector('button');
-const wrapper = document.querySelector('.squareWrapper');
+const textContainer = document.querySelector('.textContainer');
+const listContainer = document.querySelector('.listContainer');
+const html = document.querySelector('html');
 
-function createDivElement(){
-  const randColor = "#" + ("00" + Math.floor(Math.random()*16777215)).toString(16).substr(-6);
-  const divSquere = document.createElement('div');
-  divSquere.classList.add('square')
-  divSquere.style.backgroundColor = randColor;
-  divSquere.textContent = randColor;
-  wrapper.append(divSquere);
-};
-
-btn.addEventListener('click',createDivElement);
-wrapper.addEventListener('click',({target})=>{
-  if(target.classList.contains('square')){
-    console.log(target.style.backgroundColor);
-  }return;
-  
+document.addEventListener('mouseover',({target})=>{
+  const closestLi = target.closest('li');
+  if(closestLi){
+    textContainer.textContent = closestLi.textContent;
+    return
+  }
+  textContainer.textContent = closestLi.tagName;
 });
+
+
